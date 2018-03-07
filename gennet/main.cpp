1303c37dc32905c55115e3bd058ba5097b5d150b
raghavas <raghavas@thoughtworks.com>
2018-03-07 22:43:30 +0530

    Initial Commit

---
+++ gennet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gennet
+//
+//  Created by Raghava Sasikanth Goteti on 07/03/18.
+//  Copyright © 2018 Raghava Sasikanth Goteti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
